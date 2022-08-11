8e84f21b19df05af8c77938dad8465d20bbc374d
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-08-10 22:57:30 -0300

    Agregué useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Saludo from './Saludo';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const App = () => {
+ 
+  const[mensaje, setMensaje] = useState('');
+
   return (
-    <div>
-      <Saludo contenido='My Friend!'></Saludo>
+    <div className='container'>
+      <Saludo contenido='My Friend!'>{mensaje}</Saludo>
+      <Button variant="outline-warning" onClick={()=>setMensaje('(from changed state)')}>Mensaje</Button>
     </div>
   );
 };
